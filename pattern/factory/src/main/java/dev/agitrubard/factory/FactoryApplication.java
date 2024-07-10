b2fd5f1e40ca2158d4a9390a856b524821b967b9
agitrubard <demiragitrubar@gmail.com>
2024-07-10 21:22:06 +0300

    Project Has Been Initialized

---
+++ pattern/factory/src/main/java/dev/agitrubard/factory/FactoryApplication.java
@@ -0,0 +1,13 @@
+package dev.agitrubard.factory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FactoryApplication.class, args);
+	}
+
+}
